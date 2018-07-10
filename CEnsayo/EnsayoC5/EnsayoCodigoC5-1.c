89d0b21f573be603187009cdef3148db8e6df340
ricardomachorro <ricardoalbeto.machorrovences@gmail.com>
2018-07-10 17:32:58 -0500

    Actualizacion C

--- CEnsayo/EnsayoC5/EnsayoCodigoC5-1.c
+++ CEnsayo/EnsayoC5/EnsayoCodigoC5-1.c
@@ -8,6 +8,11 @@ pseudocodigo de la forma: condicion a evaluar ? pasos_si_es_verdadero : pasos_si
 int  x=34,y=24;
 
 printf("%i, %i\n", 1 ? x : y, 0 ? x : y);
+/*Este es muy util un ejemplo de este es de poder imprimir el mayor de 3 numeros*/
+int big=0,a=4,b=12,c=7;
+big= a > b ? (a > c ? a : c): (b > c ? b : c);
+printf("\n%i",big);
+
 
  return 0;
 }
