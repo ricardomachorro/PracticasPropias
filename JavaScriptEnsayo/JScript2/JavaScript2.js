436d4ef2d621925cead504f483aadf0fd97283be
ricardomachorro <ricardoalbeto.machorrovences@gmail.com>
2018-07-12 16:58:10 -0500

    Actualizacion javascript

---
+++ JavaScriptEnsayo/JScript2/JavaScript2.js
@@ -0,0 +1,5 @@
+/*Para poder declaración de varaibles en javascript se usa la palabra
+reservada ' var  ' y la asignación de la variable es:
+'   var nombre_variable="valor_variable"         ' */
+var variable1="Hello boy";
+document.write(variable1);
