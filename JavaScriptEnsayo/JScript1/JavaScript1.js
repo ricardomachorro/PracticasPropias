dd738c2676332d11e5b496a49a18a0bb805e2a4e
ricardomachorro <ricardoalbeto.machorrovences@gmail.com>
2018-07-12 16:36:06 -0500

    Actualizacion javascript

--- JavaScriptEnsayo/JScript1/JavaScript1.js
+++ JavaScriptEnsayo/JScript1/JavaScript1.js
@@ -1,1 +1,24 @@
-alert ("Elemento dw javascript desde un elemento eterno");
+/*Este es un ejemplo de un archivo externo que permite manejar
+de manera rapida elementos d ejavascript
+*/
+
+alert ("Elemento de javascript desde un elemento externo");
+
+/*Para poder dar un mesaje en la consola de un navegador se usa :
+' console.log("texto_a_escribir") '*/
+
+console.log("Texto del navegador");
+
+/*Para poder dar un mensaje de error en la consola del navegador se usa:
+'  console.error("texto_a_escribir"); '*/
+console.error("Texto de error ");
+
+/*Para dar un mensaje de informacion extra en la consola del navegador se usa:
+  '  console.info("texto_a_escribir"); '*/
+
+  console.info("Texto de informacion");
+
+/*Para poder dar un mensaje de advertencia en la consola del navevador se usa:
+   '  console.warn("tecto_a_escribir");   ' */
+
+  console.warn("Texto de advertencia");
