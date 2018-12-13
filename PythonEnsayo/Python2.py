f5f1fcdf987ccb1a568655bbae75bd362d18cef2
ricardomachorro <ricardoalbeto.machorrovences@gmail.com>
2018-12-13 13:23:52 -0600

    Python commit

--- PythonEnsayo/Python2.py
+++ PythonEnsayo/Python2.py
@@ -1,1 +1,9 @@
-"""dasdadd"""
+"""Python tambien puede tener datos ordenados en colecciones dentro
+de estas colecciones estan :
+  Listas:Es una coleccion de elementos que permite repeticion de elementos
+  Sets:Es un coleccion de elementos que no permite la repeticion de elementos
+  Diccionarios: Es una coleccion de elemtnos en pares con un elemento siendo considrrado
+  como la llave o idnetificador a cierto elemento
+
+
+"""
