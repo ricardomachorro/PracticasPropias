87edbb7918db7e351d8833df402a2bc212760445
ricardomachorro <ricardoalbeto.machorrovences@gmail.com>
2018-12-13 14:28:33 -0600

    Python commit

--- PythonEnsayo/Python2.py
+++ PythonEnsayo/Python2.py
@@ -4,6 +4,26 @@ de estas colecciones estan :
   Sets:Es un coleccion de elementos que no permite la repeticion de elementos
   Diccionarios: Es una coleccion de elemtnos en pares con un elemento siendo considrrado
   como la llave o idnetificador a cierto elemento
-
-
 """
+#Uno de estas listas son los string o conjunto de letras
+
+#Los string se declaran con entrecomilas " " o con comillas simples ' '
+a = "Hola a todos"
+print(a)
+"""Si se quiere usar string que repete los saltos de linea se les ponen entre
+   comillas dobles triples """
+b = """Respetas
+los
+espacios"""
+print(b)
+#tambien se puede selccionar que letra especifica poniendo el nombre de la variables
+# y luego poner en ella los signos " [] " y dentro de estos el numero de la posicion de
+# la letra que se requiere empezando desde el cero
+print(a[0])
+print(a[3])
+#Para repetir un texto se usa despues de esta el signo " * " seguido del numero
+#de veces que se va a repetir
+print(a*2)
+print(a*4)
+print(a[0]*2)
+print(a[0]*4)
