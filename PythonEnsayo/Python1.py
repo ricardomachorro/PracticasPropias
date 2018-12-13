f800039a1f91dce5e1f637bb65a471dd18a1811f
ricardomachorro <ricardoalbeto.machorrovences@gmail.com>
2018-12-13 12:05:51 -0600

    Python1 commit

---
+++ PythonEnsayo/Python1.py
@@ -0,0 +1,10 @@
+#La forma de comentar en python solo una linea de codigo
+# es con el signo "  #  "  ejemplo: #comentario1
+
+#Para comentar mas de una linea se usa usa el texto en entre comillas triples un
+# ejemplo: """  texto a comentar """
+"""  Texto a comentar linea1
+     Texto a comentar linea2
+     Texto a comentar linea3
+
+ """
