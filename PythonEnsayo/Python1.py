e96ca3950773607759d82d3d7c98062be013a517
ricardomachorro <ricardoalbeto.machorrovences@gmail.com>
2018-12-13 12:32:04 -0600

    Python commit

--- PythonEnsayo/Python1.py
+++ PythonEnsayo/Python1.py
@@ -8,3 +8,24 @@
      Texto a comentar linea3
 
  """
+
+"""Los tipos de datos que hay en python entre los cuales estan:
+  Numericos: int y float son algunos tipos de datos con numeros
+  List: Son conjuntos ordenados de  otros tipos de datos como String
+  Complex: Son  tipos de datos variables
+"""
+
+#Los datos Numericos pueden ser flotantes o enteros ejemplos son:
+
+
+#enteros
+a = 12
+b = 34
+c = 3
+print(a,b,c)
+print(type(a))
+
+#flotantes
+
+x = 23.56
+y = 12.4
