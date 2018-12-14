ac46c1c256e9a30e6de22fde0e4ea727ca29a415
ricardomachorro <ricardoalbeto.machorrovences@gmail.com>
2018-12-13 18:32:22 -0600

    Python commit

---
+++ Criptografia/Atbash.py
@@ -0,0 +1,9 @@
+#Cifrado atbash
+import pyperclip
+#Alfabetos empleados
+CLARO = "ABCDEFGHIJKLMNOPQRSTUVWXYZ"
+CIFRADO = "ZYXWUVTSRQPONMLKJIHGFEDCBA"
+
+salida = ''
+
+texto = input("Introduce el texto") 
