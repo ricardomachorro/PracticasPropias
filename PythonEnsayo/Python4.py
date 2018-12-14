c1ee5622746715c6ef1189f955ed442afe3616ef
ricardomachorro <ricardoalbeto.machorrovences@gmail.com>
2018-12-14 10:51:13 -0600

    actualizacion de python 4

--- PythonEnsayo/Python4.py
+++ PythonEnsayo/Python4.py
@@ -8,5 +8,15 @@
 #un integer, se pone despues de este elemento una coma " ,"
 
 #un ejemplo de una tupla:
-tupla=(1,34,24,"Rick")
+tupla=(1,34,24,"Rick",12,12)
 print(tupla)
+#al igual que una lista una tupla se puede seleccionar un elemento
+#con poniendo al final " [i] " sinedo i el indice del elemento a seleccionar
+print(tupla[2])
+#tambien se puede repetir la tupla un numero de veces con " * "
+print(tupla*4)
+#tambien se puede contar el numero de elementos que encaguen en cierta descripcion
+#con la funcion " count "
+print(tupla.count(12))
+#tambien se puede ver ele indice de un elemento con la funcion index
+print(tupla.index("Rick"))
