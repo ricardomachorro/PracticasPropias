56161fcf94030b6e4657244f51e9b5a6bebeb44c
ricardomachorro <ricardoalbeto.machorrovences@gmail.com>
2018-12-14 10:09:37 -0600

    actualizacion de python 3

--- PythonEnsayo/Python3.py
+++ PythonEnsayo/Python3.py
@@ -5,7 +5,7 @@
 # " [] " y  despues se le pone el contenido
 
 #un ejemplo de esto es:
-list=[12,12,30,"Hola",13.4]
+list=[12,12,30,"Hola",13.4,127,1]
 print(list)
 #como en un string se puede seleccionar cierta seccion de esta
 
@@ -25,3 +25,15 @@ print(len(list))
 #en ejemplo:
 list.append("Rick")
 print(list)
+#tambien se puede eliminar elementos una forma es con remove y el elemento a eliminar:
+list.remove(30)
+print(list)
+#otra forma es con la funcion " del " y el  elemento a eliminar:
+del(list[4])
+print(list)
+list2=[12,30,13.4,127,1]
+#Tambien se puede seleccionar el minimo y maximo numericos de una lista
+#para el maximo se usa la  funcion max
+print(max(list2))
+#para el minimo se usa la  funcion min
+print(min(list2))
