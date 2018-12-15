0ac614e12183023d5910b4f4fddcdac75ac1d1f3
ricardomachorro <ricardoalbeto.machorrovences@gmail.com>
2018-12-15 15:52:03 -0600

    Python 5 act

---
+++ PythonEnsayo/Python5.py
@@ -0,0 +1,18 @@
+#Una coleccion de datos que permite python es el de los "  set " , el cual
+# a diferencia de las listas no permite repetociones, pero a diferencias
+#de las tuplas permite modificacion
+
+#Para declarar los set set se hace con corchetes " { contenido }" despues de la declaracion
+#ejemplo:
+
+set1 = {12,34,22,"Yeah"}
+print(set1)
+print(type(set1))
+
+#Para poder agregar un elemento a un set se usa el metodo " update " en el cual
+# se pone como parametro entre signos " [] " el contenido a ingresar
+
+#ejemplo:
+
+set1.update([55,2,3])
+print(set1) 
