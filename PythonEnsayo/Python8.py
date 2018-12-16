1d649a5c04baf00b676d4941b015a7e3a6b68bf6
ricardomachorro <ricardoalbeto.machorrovences@gmail.com>
2018-12-16 17:19:22 -0600

    Python ct 8

--- PythonEnsayo/Python8.py
+++ PythonEnsayo/Python8.py
@@ -17,3 +17,55 @@ print("Division entera:",a%b)
 print("Exponente:",a**b)
 #Division decimal
 print("Division decimal:",a/b)
+
+#En pyhton tambien se puede dar asignaciones multiples
+d=e=f=10
+print(d,e,f)
+x,y=10,5
+#Sumar de manera mas corta
+x+=5
+print(x)
+#Restar de manera mas corta
+y-=5
+print(y)
+#Multiplicar de manera mas corta
+x*=2
+print(x)
+#dividir de manera mas corta
+x/=2
+print(x)
+#dividir de manera mas corta
+x//=2
+print(x)
+#subir un elemnto a una potencia de manera mas corta
+x**=2
+print(x)
+#obtener el modulo de un elemnto  de manera mas corta
+x%=2
+print(x)
+
+#Sus camparadores son los mismos
+
+#Igual
+print(23==2)
+#Difrente
+print(23!=2)
+#Mayor
+print(23>2)
+#Mayot o Igual
+print(23>=2)
+#Menor
+print(23<2)
+#Menor o Igual
+print(23<=2)
+
+#Una rareza de python son sun operadores logicos los cuales son:
+
+x=23
+y=24
+#and
+print((x==23 and y==23))
+#or
+print((x==24 or y==24))
+#not
+print(not(x==23 and y==23))
