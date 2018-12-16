2565ab63148c8db2938001e011663624341714d2
ricardomachorro <ricardoalbeto.machorrovences@gmail.com>
2018-12-16 16:54:04 -0600

    Python ct 8

---
+++ PythonEnsayo/Python8.py
@@ -0,0 +1,19 @@
+#Las operaciones aritmeticas basicas dque se pueden hacer con python son
+
+
+a,b = 10,5.3
+
+#Suma
+print("Suma:",a+b)
+#Resta
+print("Resta:",a-b)
+#Multiplicación
+print("Multiplicacion:",a*b)
+#División entera
+print("Division entera:",a//b)
+#Modulo
+print("Division entera:",a%b)
+#Exponente
+print("Exponente:",a**b)
+#Division decimal
+print("Division decimal:",a/b)
