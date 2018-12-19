96051de249785ffb493a0411473110ac500e23d2
ricardomachorro <ricardoalbeto.machorrovences@gmail.com>
2018-12-19 10:47:39 -0600

    Actualizacion python 9

---
+++ PythonEnsayo/Python9.py
@@ -0,0 +1,8 @@
+#La funcion print tiene varias formas de ser empleado o modificada
+#Un ejemplo es la de afectar separadores de dos varibales al imprimirlas
+
+#Ejemplo se hace con " sep " como se muestra
+a,b=12,3
+print(a,b,sep="++++")
+#Aqui sep indica con que se va a remplazar en print los signos " , " y
+#se hace con sep e igualandola al simbolo que quiere poner
