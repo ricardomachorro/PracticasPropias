01b4a8cb87718204e1e89fdd0e873364f39b2d70
ricardomachorro <ricardoalbeto.machorrovences@gmail.com>
2018-12-19 11:34:42 -0600

    Act Python 9

--- PythonEnsayo/Python9.py
+++ PythonEnsayo/Python9.py
@@ -15,4 +15,29 @@ print("Hola me llamo:",nombre," y mi edad es de :",18)
 #Otra forma es como se hace en lenhuajes como C por medio del operador " % "
 # co %s para stings ,%i para integers, %f para flotantes, y despues de la cadena a poner se pone
 #al final "%(variable1,variable2)" con todos las variables en orden a remplazar
-print()
+print("Hola me llamo:%s y mi edad es de :%i"%(nombre,edad))
+#otra forma es por medio del metodo format , donde se pone el sombolo " { } "
+#donde se desee poner la variable y al final del stirng se pone dento del metodo
+#format las variables a remplazar en orden
+print("Hola me llamo {} y mi edad es de : {}".format(nombre,edad))
+#Python tambie permite la entrada de datos con la funcion " input "
+#Para asignarle el valor de entrada del teclado solamente con la funcion input
+s=input()
+print(s)
+#Si se quiere desplegar un mensaje junto con la entrada de datos, se pone dentro
+#de la funcion input el mensaje
+d=input("Escriba su nombre:")
+print(d)
+#Para pasar puramente un dato numerico desde u input se hace con una concatenacion
+number=int(input("Escribe tu edad:"))
+print(number)
+print(type(number))
+#Python tambien puede leer multiples entradas de datos
+#Esto lo hace con la funcion " split() "
+#el metodo " split() " por default separa los elementos por espacios
+listNumber=input("Ponga unos numeros:").split()
+print(listNumber)
+#Si se quiere poner un signo en especifico a separar en especifico se pasa
+#dicho signo como parametro
+listNumber2=input("Escriba otros numeros separados por coma:").split(",")
+print(listNumber2)
