b595cf49ae6590f0aee4f42f50446f06eb2954ff
ricardomachorro <ricardoalbeto.machorrovences@gmail.com>
2018-12-25 11:19:42 -0600

    Actualizacion

--- PythonEnsayo/Python10.py
+++ PythonEnsayo/Python10.py
@@ -1,4 +1,11 @@
 #Ejemplo de programa de promedio de tres numeros con python 
+print("Ejemplos Numero 1 \n")
 a,b,c = [int(x) for x in input("Escriba 3 numeros:\n").split()]
 average = (a+b+c)/3
-print("Promedio de 3 numeros:",average) 
+print("Promedio de 3 numeros:",average)
+#Ejemplo Numero de codigo python
+#Ejemplo del area de un circulo
+radio = float(input("Escriba el area del circulo:\n"))
+Pi = 22/7
+area = pi*(radio**2)
+print("El area del circulo es:",area,"\n")
