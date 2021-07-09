b63a05ecf78340a1fc5f6b27dd34251cbf6999b1
ricardomachorro <ricardoalbeto.machorrovences@gmail.com>
2021-07-09 00:06:46 -0500

    creacion archivo tcp

--- Sockets/Java/SocketsTCP/src/Servidor.java
+++ Sockets/Java/SocketsTCP/src/Servidor.java
@@ -1,4 +1,6 @@
 
+import java.io.DataInputStream;
+import java.io.DataOutputStream;
 import java.io.IOException;
 import java.net.ServerSocket;
 import java.net.Socket;
@@ -25,10 +27,27 @@ public class Servidor {
         Socket sc=null;
         //El puerto debe ser uno muy alto para asi poder asegurar que esta libre entre uno y 65000
         final int PUERTO=5000; 
+        DataInputStream in;
+        DataOutputStream out;
 
         try{ 
         servidor=new ServerSocket(PUERTO); //Creacion de elemento de servidor del socketr
         System.out.println("Servidro iniciado");
+        
+        while(true){
+        
+          sc=servidor.accept();//Método que espera una petición dle cliente
+          //Stream de datos que recibe el servidor del socket del cliente
+          in=new DataInputStream(sc.getInputStream());
+          //Stream de datos que sca el servidor al socket dle cliente
+          out=new DataOutputStream(sc.getOutputStream());
+          
+          
+          String mensaje=in.readUTF();
+          
+        }
+        
+        
         
         }catch(IOException ex){
            
