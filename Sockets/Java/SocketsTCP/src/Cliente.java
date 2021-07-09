ed5d400c5b3b7663adefd3a710971b6c529ec48a
ricardomachorro <ricardoalbeto.machorrovences@gmail.com>
2021-07-08 19:16:51 -0500

    creacion archivo tcp

--- Sockets/Java/SocketsTCP/src/Cliente.java
+++ Sockets/Java/SocketsTCP/src/Cliente.java
@@ -3,13 +3,12 @@
  * To change this template file, choose Tools | Templates
  * and open the template in the editor.
  */
-package socketstcp;
 
 /**
  *
  * @author Ricardo Alberto
  */
-public class SocketsTCP {
+public class Cliente {
 
     /**
      * @param args the command line arguments
