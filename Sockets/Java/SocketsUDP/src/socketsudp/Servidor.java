fdea09124170cbaebb0ae921c4215d069a98a255
ricardomachorro <ricardoalbeto.machorrovences@gmail.com>
2021-07-09 20:45:23 -0500

    cliente TCP

--- Sockets/Java/SocketsUDP/src/socketsudp/Servidor.java
+++ Sockets/Java/SocketsUDP/src/socketsudp/Servidor.java
@@ -5,7 +5,10 @@
  */
 package socketsudp;
 
+import java.io.IOException;
+import java.net.DatagramPacket;
 import java.net.DatagramSocket;
+import java.net.InetAddress;
 import java.net.SocketException;
 
 /**
@@ -17,13 +20,49 @@ public class Servidor {
     /**
      * @param args the command line arguments
      */
+    
+    /*
+    
+    Los socket de datagramas usan normalmente UDP
+    esto quiere decir que no hay conexión entre
+    cleinte y servidor
+    
+    No hay handshaking
+    
+    El emisor debe indicar explícitamente la 
+    direcci+on IP  yel puerto del destino para
+    cada paquete
+    
+    Los paquetes pueden mandarse de manera aleatoria
+    y los paquetes se puedne perder
+    */
+    
+    
     public static void main(String[] args) {
         // TODO code application logic here
         final int PUERTO=5000;
-        byte[] buffer=new byte[1024];
+        byte[] buffer=new byte[1024]; //Buffer para el datagrama
         try{
            DatagramSocket socketUDP=new DatagramSocket(PUERTO);
-        }catch(SocketException ex){
+           DatagramPacket peticion=new DatagramPacket(buffer,buffer.length);//Paquete de datagramas a enviar
+           socketUDP.receive(peticion);
+            System.out.println("Recibe la infromación del cleinte");
+           String mensaje=new String(peticion.getData());
+           System.out.println(mensaje);
+           
+           int puertoCliente=peticion.getPort();
+           InetAddress direccion=peticion.getAddress();
+           
+           mensaje="Hola mundo desde el servidor";
+           buffer=mensaje.getBytes();
+           
+           DatagramPacket respuesta=new DatagramPacket(buffer,buffer.length,direccion,puertoCliente);
+            System.out.println("Enviando la información del cliente");
+           socketUDP.send(respuesta);
+           
+           
+           
+        }catch(IOException ex){
         System.out.println(ex.toString());
         }
     
