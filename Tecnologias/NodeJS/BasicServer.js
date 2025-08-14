c249a61b13d14450db0a5897d4ca765547da779c
ricardomachorro <ricardoalbeto.machorrovences@gmail.com>
2025-08-14 11:09:21 -0600

    Basic Node server creation

---
+++ Tecnologias/NodeJS/BasicServer.js
@@ -0,0 +1,12 @@
+const http = require("http")
+
+http.createServer((request,response) =>{
+
+    response.writeHead(200,{
+        'Content-Type':'text/plain'
+    });
+
+    response.write("Hello world");
+
+    response.end();
+}).listen(1337);
