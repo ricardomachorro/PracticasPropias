57f81337a583633c8eee609b14f4dc3060882d2d
ricardomachorro <ricardoalbeto.machorrovences@gmail.com>
2025-08-18 16:01:10 -0600

    Add DropAnimation

---
+++ Proyectos/DropAnimation/script.js
@@ -0,0 +1,27 @@
+const container = document.querySelector(".container");
+
+function dropped(){
+
+    const drop = document.createElement("span");
+
+    
+
+    drop.classList.add("drop");
+
+    console.log(drop);
+
+   drop.style.top = Math.random() * innerHeight + "px";
+    drop.style.left = Math.random() * innerWidth + "px";
+
+    setTimeout(()=>{
+        drop.remove();
+    },6500);
+
+
+    container.appendChild(drop);
+    
+
+
+}
+
+setInterval(dropped,500);
